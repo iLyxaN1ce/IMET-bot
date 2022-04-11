1573b02b6ed0ab02b541342917c19608e01fa5a0
ilya-pyatizbyantsev <ilya_2.0@mail.ru>
2022-04-11 03:05:32 +0300

    shift to linux

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,17 +1,30 @@
-package pck;
+package com.example;
 
+import com.bot.Bot;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
+@SpringBootApplication
+@RestController
 public class Main {
     public static void main(String[] args) {
-
         try {
             TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
             telegramBotsApi.registerBot(new Bot());
+            SpringApplication.run(Main.class, args);
         } catch (TelegramApiException e) {
             e.printStackTrace();
         }
     }
+
+    @GetMapping
+    public String hello() {
+        return "Polina idi pisai";
+    }
+
 }
